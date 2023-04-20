2b585a4d5bb428bac9e64ea67baa0f10a1af5cc5
Mauricio Chiapetta <m.faisca1234@gmail.com>
2023-04-19 22:24:20 -0300

    commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('#task');
+const taskList = document.createElement('ul');
+const todoApp = document.querySelector('.todoApp');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const task = input.value;
+  if (task !== '') {
+    addTask(task);
+    input.value = '';
+  }
+});
+
+function addTask(task) {
+  const listItem = document.createElement('li');
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'X';
+  deleteButton.className = 'deleteButton';
+
+  deleteButton.addEventListener('click', () => {
+    listItem.remove();
+  });
+  
+  listItem.innerHTML = task;
+  listItem.appendChild(deleteButton);
+  taskList.appendChild(listItem);
+  todoApp.appendChild(taskList);
+}
+
+
